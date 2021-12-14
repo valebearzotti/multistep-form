a9657474fb01bea5085cd65da10dcf5a874feda9
Valentina Bearzotti <valebearzotti1@gmail.com>
2021-12-13 21:43:39 -0300

    fixing access to card object

--- src/components/steps/StepThree.js
+++ src/components/steps/StepThree.js
@@ -9,17 +9,17 @@ function StepThree({setData, data, nextStep, prevStep}) {
         <div>
             <StepTitle>Credit card details</StepTitle>
             <Label>Name</Label>
-            <Input type="text" onChange={(e) => setData({...data, card: {...card, cardName: e.target.value}})}/>
+            <Input type="text" onChange={(e) => setData({...data, card: {...data.card, cardName: e.target.value}})}/>
             <Label>Credit card number</Label>
-            <Input type="text" onChange={(e) => setData({...data, card: {...card, cardNumber: e.target.value}})}/>
+            <Input type="text" onChange={(e) => setData({...data, card: {...data.card, cardNumber: e.target.value}})}/>
             <Label>Expiry Date</Label>
             <CreditCard>
-                <Field type="text" placeholder="MM" onChange={(e) => setData({...data, card: {...card, expiracyMonth: e.target.value}})}/>
+                <Field type="text" placeholder="MM" onChange={(e) => setData({...data, card: {...data.card, expiracyMonth: e.target.value}})}/>
                 <span>/</span>
-                <Field type="text" placeholder="YY" onChange={(e) => setData({...data, year: e.target.value})}/>
+                <Field type="text" placeholder="YY" onChange={(e) => setData({...data, card: {...data.card, expiracyYear: e.target.value}})}/>
             </CreditCard>
             <Label>CVC</Label>
-            <Input type="text" onChange={(e) => setData({...data, cvc: e.target.value})}/>
+            <Input type="text" onChange={(e) => setData({...data, card: {...data.card, cvc: e.target.value}})}/>
             <Buttons>
                 <Previous onClick={prevStep}>Previous</Previous>
                 <Next onClick={nextStep}>Send</Next>
