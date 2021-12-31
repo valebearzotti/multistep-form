1619e24bb50ef602dc5c05d13105d14f0d5b0bd0
valebearzotti <valebearzott1@gmail.com>
2021-12-31 17:21:01 -0300

    styling

--- src/components/steps/StepThree.js
+++ src/components/steps/StepThree.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StepTitle, Label, Input, Next, Previous, Buttons, CreditCard, Field } from '../../styles/Form.styled'
+import { StepTitle, Label, Input, Next, Previous, Buttons, CreditCard, Field, Fields, Expiracy } from '../../styles/Form.styled'
 
 function StepThree({setData, data, nextStep, prevStep}) {
 
@@ -12,14 +12,20 @@ function StepThree({setData, data, nextStep, prevStep}) {
             <Input type="text" onChange={(e) => setData({...data, card: {...data.card, cardName: e.target.value}})} value={data.card.cardName}/>
             <Label>Credit card number</Label>
             <Input type="text" onChange={(e) => setData({...data, card: {...data.card, cardNumber: e.target.value}})} value={data.card.cardNumber}/>
-            <Label>Expiry Date</Label>
-            <CreditCard>
-                <Field type="text" placeholder="MM" onChange={(e) => setData({...data, card: {...data.card, expiracyMonth: e.target.value}})} value={data.card.expiracyMonth}/>
-                <span>/</span>
-                <Field type="text" placeholder="YY" onChange={(e) => setData({...data, card: {...data.card, expiracyYear: e.target.value}})} value={data.card.expiracyYear}/>
-            </CreditCard>
-            <Label>CVC</Label>
-            <Input type="text" onChange={(e) => setData({...data, card: {...data.card, cvc: e.target.value}})} value={data.card.cvc}/>
+            <Fields>
+                <CreditCard>
+                    <Label>Expiry Date</Label>
+                    <Expiracy>
+                            <Field type="text" placeholder="MM" onChange={(e) => setData({...data, card: {...data.card, expiracyMonth: e.target.value}})} value={data.card.expiracyMonth}/>
+                            <span>/</span>
+                            <Field type="text" placeholder="YY" onChange={(e) => setData({...data, card: {...data.card, expiracyYear: e.target.value}})} value={data.card.expiracyYear}/>
+                    </Expiracy>
+                </CreditCard>
+                <CreditCard>
+                    <Label>CVC</Label>
+                    <Field type="number" onChange={(e) => setData({...data, card: {...data.card, cvc: e.target.value}})} value={data.card.cvc}/>
+                </CreditCard>
+            </Fields>
             <Buttons>
                 <Previous onClick={prevStep}>Previous</Previous>
                 <Next onClick={nextStep}>Send</Next>
